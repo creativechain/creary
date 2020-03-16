10f10c7fc4790829dd01a8908b369df4e292f656
ander7agar <ander7agar@gmail.com>
2020-03-16 12:12:26 +0100

    Schema default String

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
