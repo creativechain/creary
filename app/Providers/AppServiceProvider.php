d39a639abcb34aa4988044298813e0b2a6ae6640
ander7agar <ander7agar@gmail.com>
2020-05-04 20:59:59 +0200

    Fixing lang

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,6 @@ class AppServiceProvider extends ServiceProvider
 
         $lang = $request->cookie('lang');
 
-        dd($lang, $request->getPreferredLanguage(['en', 'es']));
         if (!$lang) {
             $lang = $request->getPreferredLanguage(['en', 'es']);
             if (!$lang) {
@@ -38,6 +37,7 @@ class AppServiceProvider extends ServiceProvider
             }
         }
 
+        dd($lang);
         App::setLocale($lang);
         Schema::defaultStringLength(191);
 
