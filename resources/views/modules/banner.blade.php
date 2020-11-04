40c933c4d4cd62eea2aaf3fd1246d11028ab5dbc
manolo cruzado <csmanolo@gmail.com>
2020-11-04 10:39:39 +0100

    actualizando banner

--- resources/views/modules/banner.blade.php
+++ resources/views/modules/banner.blade.php
@@ -1,20 +1,20 @@
-<section v-cloak v-if="showBanner" class="imagebg image--dark cover cover-blocks bg--secondary" id="slide-home">
+<section v-cloak v-if="showBanner" class="imagebg image--dark cover cover-blocks" id="slide-home" style="background-color: #D1DFCE;">
     <div class="row-close" v-on:click="showBanner = false">
         <a class="cursor"><i class="fas fa-times"></i></a>
     </div>
-    <div class="background-image-holder">
-        <img alt="background" src="{{ asset('img/crea-web/banner_2_6_casmiclab.jpg') }}"/>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-lg-6 offset-1">
+    <div class="container post-container-home">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-8">
                 <h1>{{ __('lang.BANNER.TITLE') }}</h1>
-                <h3>{{ __('lang.BANNER.SUBTITLE') }}</h3>
-                <a href="/welcome" class="btn btn--sm">
+                <h3 class="font-weight-normal">{{ __('lang.BANNER.SUBTITLE') }}</h3>
+                <a href="/welcome" class="btn btn--sm btn-primary">
                     <span class="btn__text font-weight-bold">
                             {{ __('lang.BUTTON.SIGN_UP') }}
                     </span>
                 </a>
+            </div>
+            <div class="col-12 col-md-4">
+                <img alt="background" src="{{ asset('img/crea-web/banner-crea-art-by-samson.png') }}"/>
             </div>
         </div>
     </div>
