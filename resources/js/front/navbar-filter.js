0d2af002aa8e71dd58958113ffdc61ac67e85dab
ander7agar <ander7agar@gmail.com>
2020-11-13 12:59:12 +0100

    fixed navbar slide

--- resources/js/front/navbar-filter.js
+++ resources/js/front/navbar-filter.js
@@ -1,4 +1,4 @@
-let cEvent = 'crea.modal.loaded';
+let cEvent = 'crea.content.loaded';
 creaEvents.on(cEvent, function () {
     console.log('NavFilter cEvent', cEvent);
     mr.sliders.documentReady($);
