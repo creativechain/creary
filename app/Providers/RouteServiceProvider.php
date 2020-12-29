dd80f665fe26b8ab84b11ed12df4c2764d776559
ander7agar <ander7agar@gmail.com>
2020-12-29 13:03:26 +0100

    Fixed rate limit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -76,7 +76,7 @@ class RouteServiceProvider extends ServiceProvider
 
     protected function configureRateLimiting() {
         RateLimiter::for('search', function (Request $request) {
-            Limit::perMinute(150);
+            return Limit::perMinute(150);
         });
     }
 }
