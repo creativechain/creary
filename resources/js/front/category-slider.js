c77782e18170ea9b7ee65861350dd7bb73bc0a57
Joaquín Reyes <hola@jrltt.net>
2021-01-11 20:51:26 +0100

    Format js file and remove not needed class

--- resources/js/front/category-slider.js
+++ resources/js/front/category-slider.js
@@ -1,21 +1,17 @@
-let categoryList,
-backArrow,
-nextArrow,
-currentCategory,
-allCategoryLinks;
+let categoryList, backArrow, nextArrow, currentCategory, allCategoryLinks;
 
 const categorySlider = function() {
   const init = function() {
-    categoryList = document.querySelector(".filter-list");
-    backArrow = document.querySelector(".action-prev a");
-    nextArrow = document.querySelector(".action-next a");
-    currentCategory = categoryList.querySelector("li.active");
-    allCategoryLinks = categoryList.querySelectorAll("li.category a");
+    categoryList = document.querySelector('.filter-list');
+    backArrow = document.querySelector('.action-prev a');
+    nextArrow = document.querySelector('.action-next a');
+    currentCategory = categoryList.querySelector('li.active');
+    allCategoryLinks = categoryList.querySelectorAll('li.category a');
 
     bindEvents();
     render();
     forceActivePosition();
-  }
+  };
 
   const bindEvents = function() {
     window.addEventListener('resize', render);
@@ -23,9 +19,9 @@ const categorySlider = function() {
     backArrow.addEventListener('click', backClicked);
     nextArrow.addEventListener('click', nextClicked);
     allCategoryLinks.forEach(function(category) {
-      category.addEventListener('click', categoryClicked)
-    })
-  }
+      category.addEventListener('click', categoryClicked);
+    });
+  };
 
   const render = function() {
     const edgeLeft = 0 < categoryList.scrollLeft;
@@ -38,17 +34,17 @@ const categorySlider = function() {
         backArrow.parentElement.classList.remove('-hide');
         backArrow.parentElement.classList.add('-show');
       }
-    } else { 
+    } else {
       if (!backArrow.parentElement.classList.contains('-hide')) {
         backArrow.parentElement.classList.add('-hide');
-				backArrow.parentElement.classList.remove('-show');
+        backArrow.parentElement.classList.remove('-show');
       }
-    } 
-      
+    }
+
     if (edgeRight) {
       if (!nextArrow.parentElement.classList.contains('-hide')) {
         nextArrow.parentElement.classList.add('-hide');
-				nextArrow.parentElement.classList.remove('-show');
+        nextArrow.parentElement.classList.remove('-show');
       }
     } else {
       if (nextArrow.parentElement.classList.contains('-hide')) {
@@ -59,38 +55,38 @@ const categorySlider = function() {
         nextArrow.parentElement.classList.add('-show');
       }
     }
-  }
+  };
 
   const nextClicked = function() {
-    categoryList.scrollLeft += categoryList.clientWidth
+    categoryList.scrollLeft += categoryList.clientWidth;
   };
-  
+
   const backClicked = function() {
-    categoryList.scrollLeft -= categoryList.clientWidth
+    categoryList.scrollLeft -= categoryList.clientWidth;
   };
 
   const categoryClicked = function($event) {
     categoryList.querySelector('li.active').classList.remove('active');
     $event.currentTarget.parentElement.classList.add('active');
-    
+
     const categoryCurrentPosition = $event.currentTarget.offsetLeft + $event.currentTarget.clientWidth;
     const edgeLimitRight = categoryList.clientWidth + categoryList.scrollLeft - nextArrow.clientWidth;
     const offsetToLeft = $event.currentTarget.offsetLeft - backArrow.clientWidth;
-    const offsetToRight = $event.currentTarget.offsetLeft - nextArrow.clientWidth ;
-		
+    const offsetToRight = $event.currentTarget.offsetLeft - nextArrow.clientWidth;
+
     if (offsetToLeft < categoryList.scrollLeft && offsetToRight) {
       categoryList.scrollLeft = offsetToRight;
     }
     if (edgeLimitRight < categoryCurrentPosition) {
       categoryList.scrollLeft = categoryList.scrollLeft + categoryCurrentPosition - edgeLimitRight;
-    } 
-  }
+    }
+  };
 
   const forceActivePosition = function() {
     const currentCatWidth = currentCategory.offsetLeft + currentCategory.clientWidth;
     const restCurrentCatWidth = categoryList.clientWidth - nextArrow.clientWidth;
     const currentCatNearToEnd = currentCategory.offsetLeft - nextArrow.clientWidth;
-    
+
     if (currentCategory && (currentCatWidth < restCurrentCatWidth || currentCatNearToEnd)) {
       categoryList.scrollLeft = currentCatNearToEnd;
     }
@@ -98,7 +94,7 @@ const categorySlider = function() {
 
   return {
     init
-  }
-}
+  };
+};
 
-export {categorySlider};
+export { categorySlider };
--- resources/views/modules/navbar-filter-home.blade.php
+++ resources/views/modules/navbar-filter-home.blade.php
@@ -45,7 +45,7 @@
                         <div class="actions action-next -hide">
                             <a href="#">
                                 <svg class="action-icon" viewBox="0 0 100 100">
-                                    <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow" transform="translate(100, 100) rotate(180) "></path>
+                                    <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path>
                                 </svg>
                             </a>
                         </div>
