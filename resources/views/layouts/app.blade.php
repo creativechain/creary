5c00a5f18ac1cdc1b11477179acdd590b8da14b0
ander7agar <ander7agar@gmail.com>
2021-02-09 15:46:19 +0100

    Increase logo size

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -379,7 +379,7 @@
                 <div class="col-2 col-md-2 col-lg-2 hidden-xs">
                     <div class="bar__module">
                         <a href="/">
-                            <img class="logo" alt="logo" v-lazy="'{{ asset('img/logo_creary.svg') }}'"/>
+                            <img class="logo" alt="logo" v-lazy="'{{ asset('img/logo_creary.svg') }}'" />
                         </a>
                     </div>
                 </div>
