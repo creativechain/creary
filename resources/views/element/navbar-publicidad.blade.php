0d2cdec9200f23f7004e97daa1512243c2b39dc9
ander7agar <ander7agar@gmail.com>
2021-03-12 17:56:01 +0000

    Maquetacion cuenta atras

---
+++ resources/views/element/navbar-publicidad.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <p class="text-white">Skynet Creative Challenge - $750 rewards - 
+                <a href="https://creary.net/skynet/@creary/skynet-creative-challenge" class="text-white">Join now</a>
+                <div id="countdown"></div>
+            </p>
+        </div>
+    </div>
+</div>
+<a href="" class="close"><i class="text-white fas fa-times"></i></a>
