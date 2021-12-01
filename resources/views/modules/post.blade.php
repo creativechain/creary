e61faf38f88d41ec05033e39a9f557db83008225
csmanolo <csmanolo@gmail.com>
2021-12-01 20:28:00 +0000

    fix

--- resources/views/modules/post.blade.php
+++ resources/views/modules/post.blade.php
@@ -90,7 +90,7 @@
                                                 <p class="error-color-form">{{ __('lang.HOME.DROPDOWN_REFUSED_PAYOUT') }} <span class="refused-payout">@{{ getPayout() }}</span></p>
                                             </div>
                                             <div v-else class="col-12 col-sm-12 col-md-12 dropdown__content amount-post-view-home">
-                                                <a class="delete-btn">Delete project</p>
+                                                <a class="delete-btn">Delete project</a>
                                             </div>
                                         </div><!--end row-->
                                     </div><!--end container-->
